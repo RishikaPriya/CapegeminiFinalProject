55b6dcbf4c80495006ded736aa32305674ae7fae
RishikaPriya <rishikapriya2895@gmail.com>
2016-09-30 09:38:45 +0530

    Update EmployeeServiceImpl.java

--- FPL Project/src/com/flp/ems/service/EmployeeServiceImpl.java
+++ FPL Project/src/com/flp/ems/service/EmployeeServiceImpl.java
@@ -1,5 +1,8 @@
 package com.flp.ems.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -13,26 +16,30 @@ import com.flp.ems.domain.Role;
 public class EmployeeServiceImpl implements IEmployeeService {
 
 	EmployeeDaoImp employeeDAO = new EmployeeDaoImp();
-
+	
 	@Override
-	public void addEmployee(HashMap<String, Object> employee) {
+	public void addEmployee(HashMap<String, Object> employee) throws ParseException {
+		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+	
 		Employee emp = new Employee((String) employee.get("name"), (String) employee.get("address"),
-				(Long) employee.get("phoneNumber"), (Date) employee.get("dob"), (Date) employee.get("joiningDate"),
+				(Integer) employee.get("phoneNumber"), dateFormat.parse((String) employee.get("dob")),dateFormat.parse((String)employee.get("joiningDate")),
 				this.getDepartment((Integer) employee.get("deptId")), this.getRole((Integer) employee.get("roleId")),
 				this.getProject((Integer) employee.get("projectId")));
+		
+		employeeDAO.addEmployee(emp);
 
 	}
 
+	private Department getDepartment(Integer integer) {
+		return new Department(505, "Service Layer", "BAPI");
+	}
+	
 	private Role getRole(Integer integer) {
 		return new Role(101, "Develop", "Developer");
 	}
 
 	private Project getProject(Integer integer) {
 		return new Project(302, "Management System", "EMS", getDepartment(505));
-	}
-
-	private Department getDepartment(Integer integer) {
-		return new Department(505, "Service Layer", "BAPI");
 	}
 
 	@Override
