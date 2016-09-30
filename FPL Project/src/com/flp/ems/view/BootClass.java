5094e5d60550fbdadfb81700899686cb0eb85144
RishikaPriya <rishikapriya2895@gmail.com>
2016-09-30 08:36:11 +0530

    Update BootClass.java

--- FPL Project/src/com/flp/ems/view/BootClass.java
+++ FPL Project/src/com/flp/ems/view/BootClass.java
@@ -1,29 +1,39 @@
 package com.flp.ems.view;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class BootClass {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Scanner input = new Scanner(System.in);
-				
-		System.out.println("Employee Management System");
-		System.out.println("1.Add Employee\n2.Modify Employee\n3.Remove Employee\n4.Search Employee\n5.Display all Employee");
-		System.out.println("Enter choice: ");
-		int choice = input.nextInt();
+		UserInteraction userInteraction = new UserInteraction();
 		
-		switch(choice){
-			case 1 : UserInteraction.addEmployee();
-					 break;
-			case 2 : UserInteraction.modifyEmployee();
-			 		 break;
-			case 3 : UserInteraction.removeEmployee();
-	 		 		 break;
-			case 4 : UserInteraction.searchEmployee();
-	 		 		 break;
-			case 5 : UserInteraction.getAllEmployee();
-					 break;
-			default : System.out.println("Inavlid choice.");
-		}
+		int choice;
+		char ch;
+		
+		System.out.println("Employee Management System");
+		
+		do{
+			System.out.println("1.Add Employee\n2.Modify Employee\n3.Remove Employee\n4.Search Employee\n5.Display all Employee");
+			System.out.println("Enter choice: ");
+			choice = input.nextInt();
+
+			switch(choice){
+				case 1 : userInteraction.addEmployee();
+						 break;
+				case 2 : userInteraction.modifyEmployee();
+						 break;
+				case 3 : userInteraction.removeEmployee();
+						 break;
+				case 4 : userInteraction.searchEmployee();
+						 break;
+				case 5 : userInteraction.getAllEmployee();
+						 break;
+				default : System.out.println("Inavlid choice.");
+			}
+			System.out.println("Do you want to perform more operation (y/n): ");
+			ch= input.nextLine().charAt(0);
+		}while(ch=='y' || ch=='Y');
 	}
 }
